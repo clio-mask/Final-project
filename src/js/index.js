1ec289b96363777d4de39dc67d9cd2b97da44683
clio-mask <Amica.missa@gmail.com>
2024-04-03 17:35:39 +0300

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -29,7 +29,7 @@ window.addEventListener('DOMContentLoaded', () => {
   resizableSwiper('(max-width: 450px)', '.brands__swiper', {
     loop: true,
     spaceBetween: -60,
-    slidesPerView: 1,
+    brandSlidesPerView: 1,
     pagination: {
       el: '.brands__swiper-pagination',
       clickable: true
@@ -39,7 +39,7 @@ window.addEventListener('DOMContentLoaded', () => {
     resizableSwiper('(max-width: 450px)', '.types__swiper', {
       loop: true,
       spaceBetween: -60,
-      slidesPerView: 1,
+      brandSlidesPerView: 1,
       pagination: {
         el: '.types__swiper-pagination',
         clickable: true
@@ -48,51 +48,130 @@ window.addEventListener('DOMContentLoaded', () => {
     })
 })
 
-let slides = document.querySelectorAll('.brands__swiper-slide')
+let brandSlides = document.querySelectorAll('.brands__swiper-slide')
+let typesSlides = document.querySelectorAll('.types__swiper-slide')
+let readMore = document.querySelector('.main__text-item')
 
 const tabletMediaQuery = window.matchMedia('(768px <= width < 1360px)')
 const pcMediaQuery = window.matchMedia('(width >= 1360px)')
 
 if (tabletMediaQuery.matches) {
-  for (let i = 6; i < slides.length; i++) {
-    let slide = slides[i]
-    slide.classList.add('brands__swiper-slide--hidden')
+  for (let i = 6; i < brandSlides.length; i++) {
+    let brandSlide = brandSlides[i]
+    brandSlide.classList.add('swiper-slide--hidden')
+  }
+  for (let i = 3; i < typesSlides.length; i++) {
+    let typesSlide = typesSlides[i]
+    typesSlide.classList.add('swiper-slide--hidden')
   }
 }
 if (pcMediaQuery.matches) {
-  for (let i = 8; i < slides.length; i++) {
-    let slide = slides[i]
-    slide.classList.add('brands__swiper-slide--hidden')
+  for (let i = 8; i < brandSlides.length; i++) {
+    let brandSlide = brandSlides[i]
+    brandSlide.classList.add('swiper-slide--hidden')
+  }
+  for (let i = 4; i < typesSlides.length; i++) {
+    let typesSlide = typesSlides[i]
+    typesSlide.classList.add('swiper-slide--hidden')
   }
 }
 
-let button = document.querySelector('.brands__show-more-button')
-button.addEventListener('click', function () {
-  const isOpen = button.classList.contains('brands__show-more-button--open')
+let buttons = document.querySelectorAll('.show-more-button')
+let readButton = buttons[0]
+let brandButton = buttons[1]
+let typesButton = buttons[2]
+
+readButton.addEventListener('click', function () {
+  const isOpen = readButton.classList.contains('show-more-button--open')
 
   if (!isOpen) {
-    for (let i = 0; i < slides.length; i++) {
-      let slide = slides[i]
-      slide.classList.remove('brands__swiper-slide--hidden')
+    readMore.style.WebkitLineClamp = '100'
+    readButton.textContent = 'Скрыть'
+    readButton.classList.add('show-more-button--open')
+  } else {
+    readMore.style.WebkitLineClamp = '2'
+
+    readButton.textContent = 'Читать далее'
+    readButton.classList.remove('show-more-button--open')
+  }
+})
+
+brandButton.addEventListener('click', function () {
+  const isOpen = brandButton.classList.contains('show-more-button--open')
+
+  if (!isOpen) {
+    for (let i = 0; i < brandSlides.length; i++) {
+      let brandSlide = brandSlides[i]
+      brandSlide.classList.remove('swiper-slide--hidden')
     }
 
-    button.textContent = 'Скрыть'
-    button.classList.add('brands__show-more-button--open')
+    brandButton.textContent = 'Скрыть'
+    brandButton.classList.add('show-more-button--open')
   } else {
     if (tabletMediaQuery.matches) {
-      for (let i = 6; i < slides.length; i++) {
-        let slide = slides[i]
-        slide.classList.add('brands__swiper-slide--hidden')
+      for (let i = 6; i < brandSlides.length; i++) {
+        let brandSlide = brandSlides[i]
+        brandSlide.classList.add('swiper-slide--hidden')
       }
     }
     if (pcMediaQuery.matches) {
-      for (let i = 8; i < slides.length; i++) {
-        let slide = slides[i]
-        slide.classList.add('brands__swiper-slide--hidden')
+      for (let i = 8; i < brandSlides.length; i++) {
+        let brandSlide = brandSlides[i]
+        brandSlide.classList.add('swiper-slide--hidden')
       }
     }
 
-    button.textContent = 'Показать всё'
-    button.classList.remove('brands__show-more-button--open')
+    brandButton.textContent = 'Показать всё'
+    brandButton.classList.remove('show-more-button--open')
   }
+})
+
+typesButton.addEventListener('click', function () {
+  const isOpen = typesButton.classList.contains('show-more-button--open')
+
+  if (!isOpen) {
+    for (let i = 0; i < typesSlides.length; i++) {
+      let typesSlide = typesSlides[i]
+      typesSlide.classList.remove('swiper-slide--hidden')
+    }
+
+    typesButton.textContent = 'Скрыть'
+    typesButton.classList.add('show-more-button--open')
+  } else {
+    if (tabletMediaQuery.matches) {
+      for (let i = 3; i < typesSlides.length; i++) {
+        let typesSlide = typesSlides[i]
+        typesSlide.classList.add('swiper-slide--hidden')
+      }
+    }
+    if (pcMediaQuery.matches) {
+      for (let i = 4; i < typesSlides.length; i++) {
+        let typesSlide = typesSlides[i]
+        typesSlide.classList.add('swiper-slide--hidden')
+      }
+    }
+
+    typesButton.textContent = 'Показать всё'
+    typesButton.classList.remove('show-more-button--open')
+  }
+})
+
+let menuButton = document.querySelector('.header__menu')
+let menu = document.querySelector('.menu')
+let closeButton = document.querySelector('.menu__button')
+let modalShadow = document.querySelector('.page__modal-bg')
+
+menuButton.addEventListener('click', function () {
+  menu.classList.add('menu--open')
+  modalShadow.style.display = 'block'
+})
+
+closeButton.addEventListener('click', () => {
+  menu.classList.remove('menu--open')
+  modalShadow.style.display = 'none'
+})
+
+modalShadow.addEventListener('click', () => {
+  menu.classList.remove('menu--open')
+  modalShadow.style.display = 'none'
 })
